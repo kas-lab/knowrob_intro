c96937dec5392afdeb5abda4887d02456b3f110f
Gustavo <g.rezendesilva@tudelft.nl>
2023-08-18 17:58:10 +0200

    fix milk name, formatting, and add query to get mass value

--- scripts/example_node.py
+++ scripts/example_node.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 
-import roslib; roslib.load_manifest('rosprolog')
+import roslib
 import rospy
 from knowrob_intro.prolog_query import PrologQuery
 
+roslib.load_manifest('rosprolog')
 
-#--------------- INITIALIZE node, quering class and database ------------------------
+# --------------- INITIALIZE node, quering class and database ----------------
 
-rospy.init_node('example_query_owl') # initialize node
-pq = PrologQuery() # create a prolog query class instance
+rospy.init_node('example_query_owl')  # initialize node
+pq = PrologQuery()  # create a prolog query class instance
 
 # load example.owl in the knowrob database to access its content
-query = pq.prolog_query("load_owl('package://knowrob_intro/owl/krr_exercise.owl',\
+query = pq.prolog_query(
+    "load_owl('package://knowrob_intro/owl/krr_exercise.owl',\
      [namespace(pap, 'http://www.airlab.org/tiago/pick-and-place#')]).")
-    
-#------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------
 
 # Quering examples
 
@@ -36,8 +38,11 @@ print("Individuals with mass value:")
 pq.get_all_solutions(query)
 
 # (Find) which individuals are mass attribute of milk product 1
-query = pq.prolog_query("holds(pap:'milk_product_1', soma:hasMassAttribute, X)")
-print("Mass attribute of milk product 1:")
+query = pq.prolog_query("holds(pap:'milk1', soma:hasMassAttribute, X)")
+print("Mass attribute of milk1:")
+pq.get_all_solutions(query)
+query = pq.prolog_query("holds(pap:weight_value_milk, soma:hasMassValue, X)")
+print("Value of mass attribute of milk1:")
 pq.get_all_solutions(query)
 
 # (Find) all instances of Food
@@ -50,7 +55,3 @@ print("Not formated solution")
 print(s)
 print("\nFirst element of solution")
 print(s[0])
-
-
-
-
